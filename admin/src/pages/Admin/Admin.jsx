9492c5a14907820b1c2e1f8405d29f7dbb963047
Leanne <116819951+liy4nn4@users.noreply.github.com>
2025-01-06 06:30:21 +0800

    Update Admin.jsx

--- admin/src/pages/Admin/Admin.jsx
+++ admin/src/pages/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import AdminNavbar from '../../components/AdminNavbar/AdminNavbar'
+//import AdminNavbar from '../../components/AdminNavbar/AdminNavbar'
 
 const Admin = () => {
   return (
